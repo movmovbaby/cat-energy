fb9a5c12cbd923a8f3226cb560ccc2a09a466793
movmovbaby <dubdubman@ya.ru>
2021-01-20 08:01:29 +0300

    исправил пути для изображений и шрифтов

--- source/js/script.js
+++ source/js/script.js
@@ -4,15 +4,15 @@ const navToggle = pageHeaderWrapper.querySelector(".page-header__nav-toggle");
 const mainNav = pageHeaderWrapper.querySelector(".main-nav ");
 
 navToggle.addEventListener("click", function () {
-  if (pageHeaderWrapper.classList.contains("page-header__nav--closed")) {
-    pageHeaderWrapper.classList.remove("page-header__nav--closed");
-    pageHeaderWrapper.classList.add("page-header__nav--opened");
+  if (pageHeaderWrapper.classList.contains("page-header__wrapper--nav-closed")) {
+    pageHeaderWrapper.classList.remove("page-header__wrapper--nav-closed");
+    pageHeaderWrapper.classList.add(".page-header__wrapper--nav-opened");
     mainNav.classList.remove("main-nav--closed");
     mainNav.classList.add("main-nav--opened");
   }
-  else if (pageHeaderWrapper.classList.contains("page-header__nav--opened")) {
-    pageHeaderWrapper.classList.remove("page-header__nav--opened");
-    pageHeaderWrapper.classList.add("page-header__nav--closed");
+  else if (pageHeaderWrapper.classList.contains(".page-header__wrapper--nav-opened")) {
+    pageHeaderWrapper.classList.remove(".page-header__wrapper--nav-opened");
+    pageHeaderWrapper.classList.add("page-header__wrapper--nav-closed");
     mainNav.classList.remove("main-nav--opened");
     mainNav.classList.add("main-nav--closed");
   }
