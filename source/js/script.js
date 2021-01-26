f0118a15bd48d48734d8b7c5c061a1027460d52e
movmovbaby <dubdubman@ya.ru>
2021-01-26 22:33:37 +0300

    меню без жс с кнопкой

--- source/js/script.js
+++ source/js/script.js
@@ -4,6 +4,10 @@ const navToggle = pageHeaderWrapper.querySelector(".page-header__nav-toggle");
 const mainNav = pageHeaderWrapper.querySelector(".main-nav ");
 
 pageHeaderWrapper.classList.remove("page-header__wrapper--nav-nojs")
+pageHeaderWrapper.classList.remove("page-header__wrapper--nav-opened");
+pageHeaderWrapper.classList.add("page-header__wrapper--nav-closed");
+mainNav.classList.remove("main-nav--opened");
+mainNav.classList.add("main-nav--closed");
 
 navToggle.addEventListener("click", function () {
   if (pageHeaderWrapper.classList.contains("page-header__wrapper--nav-closed")) {
